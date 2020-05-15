729a9d603d9f9d286ae6613b7546f2111291f715
Trevor F. Smith <trevor@trevor.smith.name>
2020-05-15 08:33:13 -0700

    Switch from tabs to spaces.

--- README.md
+++ README.md
@@ -1,42 +1,42 @@
 # Babel Plugin: Annotate React
 ​
+### Status: This is a work-in-progress and is not yet ready for production use.
+
 This repo will hold a Babel plugin that annotates React components with stable attributes that can be used to search and select using FullStory. This is most useful when using a React system that generates dynamic names for Components.
 
 For React on the web the attributes are `data-component` and `data-element`.
 
 Example input:
 
-	class HelloComponent extends Component {
-		render() {
-			return <div>
-				<h1>Hello world</h1>
-			</div>;
-		}
-	}
+    class HelloComponent extends Component {
+      render() {
+        return <div>
+          <h1>Hello world</h1>
+        </div>;
+      }
+    }
 
 Example output:
 
-	class HelloComponent extends Component {
-		render() {
-			return React.createElement("div", {
-				"data-element": "div",
-				"data-component": "HelloComponent"
-			}, React.createElement("h1", {
-				"data-element": "h1"
-			}, "Hello world"));
-		}
-	}
+    class HelloComponent extends Component {
+      render() {
+        return React.createElement("div", {
+          "data-element": "div",
+          "data-component": "HelloComponent"
+        }, React.createElement("h1", {
+          "data-element": "h1"
+        }, "Hello world"));
+      }
+    }
 
 
 For React Native the attributes are `dataComponent` and `dataElement`.
 
 To activate React Native support you must pass in a `native` plugin option like so:
 
-	plugins: [
-		["@fullstory/fullstory-babel-plugin-annotate-react", { native: true }]
-	]
+    plugins: [
+      ["@fullstory/fullstory-babel-plugin-annotate-react", { native: true }]
+    ]
 
-
-### Status: This is a work-in-progress and is not yet ready for production use.
 
 Much of the logic for adding the attributes originated in the [transform-react-qa-classes](https://github.com/davesnx/babel-plugin-transform-react-qa-classes/) plugin.
--- index.js
+++ index.js
@@ -6,113 +6,113 @@ const nativeElementName = 'dataElement';
 const nativeOptionName = 'native';
 
 module.exports = function({ types: t }) {
-	return {
-		visitor: {
-			FunctionDeclaration(path, state) {
-				if (!path.node.id || !path.node.id.name) return
-				const componentName = path.node.id.name
-				let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
-				functionBodyPushAttributes(t, path, componentName, elementAttributeName)
-			},
-			ArrowFunctionExpression(path, state) {
-				if (!path.parent.id || !path.parent.id.name) return
-				const componentName = path.parent.id.name
-				let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
-				functionBodyPushAttributes(t, path, componentName, elementAttributeName)
-			},
-			ClassDeclaration(path, state) {
-				const name = path.get('id')
-				const properties = path.get('body').get('body')
-				const render = properties.find(prop => {
-					return (
-						prop.isClassMethod() &&
-						prop.get('key').isIdentifier({ name: 'render' })
-					)
-				})
-				if (!render || !render.traverse) return
+  return {
+    visitor: {
+      FunctionDeclaration(path, state) {
+        if (!path.node.id || !path.node.id.name) return
+        const componentName = path.node.id.name
+        let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
+        functionBodyPushAttributes(t, path, componentName, elementAttributeName)
+      },
+      ArrowFunctionExpression(path, state) {
+        if (!path.parent.id || !path.parent.id.name) return
+        const componentName = path.parent.id.name
+        let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
+        functionBodyPushAttributes(t, path, componentName, elementAttributeName)
+      },
+      ClassDeclaration(path, state) {
+        const name = path.get('id')
+        const properties = path.get('body').get('body')
+        const render = properties.find(prop => {
+          return (
+            prop.isClassMethod() &&
+            prop.get('key').isIdentifier({ name: 'render' })
+          )
+        })
+        if (!render || !render.traverse) return
 
-				let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
-				render.traverse({
-					ReturnStatement(returnStatement) {
-						const arg = returnStatement.get('argument')
-						if (!arg.isJSXElement()) return
-						processJSXElement(t, arg, elementAttributeName)
-						const openingElement = arg.get('openingElement')
-						if (isReactFragment(openingElement)) return
-						openingElement.node.attributes.push(
-							t.jSXAttribute(
-								t.jSXIdentifier(parentAttributeName),
-								t.stringLiteral((name.node && name.node.name) || 'unknown')
-							)
-						)
-					}
-				})
-			},
-		}
-	}
+        let [parentAttributeName, elementAttributeName] = attributeNamesFromState(state)
+        render.traverse({
+          ReturnStatement(returnStatement) {
+            const arg = returnStatement.get('argument')
+            if (!arg.isJSXElement()) return
+            processJSXElement(t, arg, elementAttributeName)
+            const openingElement = arg.get('openingElement')
+            if (isReactFragment(openingElement)) return
+            openingElement.node.attributes.push(
+              t.jSXAttribute(
+                t.jSXIdentifier(parentAttributeName),
+                t.stringLiteral((name.node && name.node.name) || 'unknown')
+              )
+            )
+          }
+        })
+      },
+    }
+  }
 }
 
 function attributeNamesFromState(state) {
-	if(state.opts[nativeOptionName] === true) {
-		return [nativeParentName, nativeElementName]
-	}
-	return [webParentName, webElementName]
+  if(state.opts[nativeOptionName] === true) {
+    return [nativeParentName, nativeElementName]
+  }
+  return [webParentName, webElementName]
 }
 
 function isReactFragment(openingElement) {
-	return (
-		openingElement.node.name.name === 'Fragment' ||
-		(openingElement.node.name.type === 'JSXMemberExpression' &&
-			openingElement.node.name.object.name === 'React' &&
-			openingElement.node.name.property.name === 'Fragment')
-	)
+  return (
+    openingElement.node.name.name === 'Fragment' ||
+    (openingElement.node.name.type === 'JSXMemberExpression' &&
+      openingElement.node.name.object.name === 'React' &&
+      openingElement.node.name.property.name === 'Fragment')
+  )
 }
 
 function addAttributesToJSXElement(t, jsxElement, elementAttributeName) {
-	if (!jsxElement || !jsxElement.node || !jsxElement.node.name) {
-		return
-	}
-	jsxElement.node.attributes = jsxElement.node.attributes || []
-	jsxElement.node.attributes.push(
-		t.jSXAttribute(
-			t.jSXIdentifier(elementAttributeName),
-			t.stringLiteral(jsxElement.node.name.name || 'unknown')
-		)
-	)
+  if (!jsxElement || !jsxElement.node || !jsxElement.node.name) {
+    return
+  }
+  jsxElement.node.attributes = jsxElement.node.attributes || []
+  jsxElement.node.attributes.push(
+    t.jSXAttribute(
+      t.jSXIdentifier(elementAttributeName),
+      t.stringLiteral(jsxElement.node.name.name || 'unknown')
+    )
+  )
 }
 
 function processJSXElement(t, jsxElement, elementAttributeName) {
-	if (!jsxElement) {
-		return
-	}
-	addAttributesToJSXElement(t, jsxElement.get('openingElement'), elementAttributeName)
-	const children = jsxElement.get('children')
-	if (children && children.length) {
-		children.forEach(element => processJSXElement(t, element, elementAttributeName))
-	}
+  if (!jsxElement) {
+    return
+  }
+  addAttributesToJSXElement(t, jsxElement.get('openingElement'), elementAttributeName)
+  const children = jsxElement.get('children')
+  if (children && children.length) {
+    children.forEach(element => processJSXElement(t, element, elementAttributeName))
+  }
 }
 
 function functionBodyPushAttributes(t, path, componentName, elementAttributeName) {
-	let jsxElement = null
-	const functionBody = path.get('body').get('body')
-	if (functionBody.parent && functionBody.parent.type === 'JSXElement') {
-		const maybeJsxElement = functionBody.find(c => {
-			return c.type === 'JSXElement'
-		})
-		if (!maybeJsxElement) return
-		jsxElement = maybeJsxElement
-	} else {
-		const returnStatement = functionBody.find(c => {
-			return c.type === 'ReturnStatement'
-		})
-		if (!returnStatement) {
-			return
-		}
-		const arg = returnStatement.get('argument')
-		if (!arg || !arg.isJSXElement()) {
-			return
-		}
-		jsxElement = arg
-	}
-	processJSXElement(t, jsxElement, elementAttributeName)
+  let jsxElement = null
+  const functionBody = path.get('body').get('body')
+  if (functionBody.parent && functionBody.parent.type === 'JSXElement') {
+    const maybeJsxElement = functionBody.find(c => {
+      return c.type === 'JSXElement'
+    })
+    if (!maybeJsxElement) return
+    jsxElement = maybeJsxElement
+  } else {
+    const returnStatement = functionBody.find(c => {
+      return c.type === 'ReturnStatement'
+    })
+    if (!returnStatement) {
+      return
+    }
+    const arg = returnStatement.get('argument')
+    if (!arg || !arg.isJSXElement()) {
+      return
+    }
+    jsxElement = arg
+  }
+  processJSXElement(t, jsxElement, elementAttributeName)
 }
