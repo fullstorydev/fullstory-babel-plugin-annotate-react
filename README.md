ae88f202335d5b1c90b659095838676eaacb5e44
Trevor F. Smith <trevor@transmutable.com>
2020-05-15 09:15:44 -0700

    Initial frame-out (#1)
    * Framed out the plugin with existing logic
    * Ported existing tests over to Jest
    * Added an option for React-Native-friendly attribute names

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
 # Babel Plugin: Annotate React
 ​
-This repo will hold a Babel plugin that annotates React components with two attributes: `data-component` and `data-element`. These attribues will allow you to use selectors to more accurately target React-rendered components when testing and when emitting Fullstory recording events.
-​
-This plugin will work with both web and native React applications.  
-​
-* Status: This is a work-in-progress and is not yet ready for production use. *
+### Status: This is a work-in-progress and is not yet ready for production use.
+
+This repo will hold a Babel plugin that annotates React components with stable attributes that can be used to search and select using FullStory. This is most useful when using a React system that generates dynamic names for Components or rearranges elements.
+
+For React on the web the attributes are `data-component` and `data-element`. For React Native the attributes are `dataComponent` and `dataElement`.
+
+The component attribute names the React.Component and the element attribute names either a web DOM element like `h1` or `input` or a native element like `View` or `Image`.
+
+Example input:
+
+    class HelloComponent extends Component {
+      render() {
+        return <div>
+          <h1>Hello world</h1>
+        </div>;
+      }
+    }
+
+Example output:
+
+    class HelloComponent extends Component {
+      render() {
+        return React.createElement("div", {
+          "data-element": "div",
+          "data-component": "HelloComponent"
+        }, React.createElement("h1", {
+          "data-element": "h1"
+        }, "Hello world"));
+      }
+    }
+
+
+To activate React Native support you must pass in a `native` plugin option like so:
+
+    plugins: [
+      ["@fullstory/fullstory-babel-plugin-annotate-react", { native: true }]
+    ]
+
+
+Much of the logic for adding the attributes originated in the [transform-react-qa-classes](https://github.com/davesnx/babel-plugin-transform-react-qa-classes/) plugin.
---
+++ __tests__/index-test.js
@@ -0,0 +1,556 @@
+const babel = require('babel-core');
+const plugin = require('../');
+const assert = require('assert');
+
+it('component-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+class componentName extends Component {
+  render() {
+    return <Fragment>A</Fragment>;
+  }
+}
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('component-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+class componentName extends Component {
+  render() {
+    return <React.Fragment>A</React.Fragment>;
+  }
+}
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+it('arrow-noreturn-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+const componentName = () => (
+  <Fragment>
+    <h1>Hello world</h1>
+  </Fragment>
+);
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('rawfunction-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+function SubComponent() {
+  return <Fragment>Sub</Fragment>;
+}
+
+const componentName = () => {
+  return <Fragment>
+    <SubCoponent />
+  </Fragment>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => {
+  return <div>
+    <h1>Hello world</h1>
+  </div>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('option-attribute snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => {
+  return <div>
+    <h1>Hello world</h1>
+  </div>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('component snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+class componentName extends Component {
+  render() {
+    return <div>
+        <h1>Hello world</h1>
+      </div>;
+  }
+}
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('rawfunction-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+function SubComponent() {
+  return <React.Fragment>Sub</React.Fragment>;
+}
+
+const componentName = () => {
+  return <React.Fragment>
+    <SubCoponent />
+  </React.Fragment>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-anonymous-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => {
+  return (() => <React.Fragment>
+    <h1>Hello world</h1>
+  </React.Fragment>)();
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-noreturn snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+);
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-noreturn-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => (
+  <React.Fragment>
+    <h1>Hello world</h1>
+  </React.Fragment>
+);
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('tags snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, UIManager } from 'react-native';
+
+UIManager.getViewManagerConfig('RCTView').NativeProps.fsClass = "String";
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return <Image source={pic} style={{ width: 193, height: 110, marginTop: 10 }} fsClass="test-class" />;
+  }
+}
+
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return <View style={{ padding: 10 }}>
+        <TextInput style={{
+        backgroundColor: '#000',
+        color: '#eee',
+        padding: 8
+      }} placeholder="Type here to translate!" // not supported on iOS
+      onChangeText={text => this.setState({ text })} value={this.state.text} />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map(word => word && '🍕').join(' ')}
+        </Text>
+      </View>;
+  }
+}
+
+export default function App() {
+  return <View style={styles.container}>
+      <Text style={{ color: '#eee' }}>FullStory ReactNative testing app</Text>
+      <Bananas />
+      <PizzaTranslator />
+    </View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('option-format snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => {
+  return <div>
+    <h1>Hello world</h1>
+  </div>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('pureComponent-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React from 'react';
+
+class PureComponentName extends React.PureComponent {
+    render() {
+        return <React.Fragment>
+            <h1>Hello world</h1>
+        </React.Fragment>;
+    }
+}
+
+export default PureComponentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('rawfunction snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+function SubComponent() {
+  return <div>Sub</div>;
+}
+
+const componentName = () => {
+  return <div>
+    <SubCoponent />
+  </div>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+const componentName = () => {
+  return <Fragment>
+    <h1>Hello world</h1>
+  </Fragment>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-react-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+const componentName = () => {
+  return <React.Fragment>
+    <h1>Hello world</h1>
+  </React.Fragment>;
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('nonJSX snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component } from 'react';
+
+class TestClass extends Component {
+  test() {
+    return true;
+  }
+}
+
+export default TestClass;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('pureComponent-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Fragment } from 'react';
+
+class PureComponentName extends React.PureComponent {
+    render() {
+        return <Fragment>
+            <h1>Hello world</h1>
+        </Fragment>;
+    }
+}
+
+export default PureComponentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('arrow-anonymous-fragment snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+const componentName = () => {
+  return (() => <Fragment>
+    <h1>Hello world</h1>
+  </Fragment>)();
+};
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('pure snapshot matches', () => {
+  const { code } = babel.transform(
+`import React from 'react';
+
+class PureComponentName extends React.PureComponent {
+    render() {
+        return <div>
+            <h1>Hello world</h1>
+        </div>;
+    }
+}
+
+export default PureComponentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [plugin]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('component-fragment-native snapshot matches', () => {
+  const { code } = babel.transform(
+`import React, { Component, Fragment } from 'react';
+
+class componentName extends Component {
+  render() {
+    return <Fragment>A</Fragment>;
+  }
+}
+
+export default componentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [
+        [plugin, { native: true }]
+      ]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
+it('pure-native snapshot matches', () => {
+  const { code } = babel.transform(
+`import React from 'react';
+
+class PureComponentName extends React.PureComponent {
+    render() {
+        return <div>
+            <h1>Hello world</h1>
+        </div>;
+    }
+}
+
+export default PureComponentName;
+`,
+    {
+      presets: ["@babel/preset-react"],
+      plugins: [
+        [plugin, { native: true }]
+      ]
+    },
+  );
+  expect(code).toMatchSnapshot();
+});
+
+
