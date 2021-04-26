123535e1b6a2cfb91469554bc21f3a062b71b2dc
Omri Vardi <omri.vardi@scylladb.com>
2021-04-26 10:34:27 +0300

    use material-ui/styles

--- samples/theming-app/src/App.jsx
+++ samples/theming-app/src/App.jsx
@@ -1,17 +1,25 @@
-import './App.css';
+import "./App.css";
 
-import DemoBox from './demoComponent'
-import { ThemeProvider } from 'theming';
+import DemoBox from "./demoComponent";
+import jss from "jss";
+import {
+  ThemeProvider,
+  StylesProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import jssPresetDefault from "jss-preset-default";
 
-const theme = { 
-  color: 'white',
-  background: 'black',
+jss.setup(jssPresetDefault());
+const theme = createMuiTheme({ colors: { red: "red" } });
+
+const App = () => {
+  return (
+    <StylesProvider jss={jss}>
+      <ThemeProvider theme={theme}>
+        <DemoBox text="this is a text" />
+      </ThemeProvider>
+    </StylesProvider>
+  );
 };
-
-  const App = () => {
-    return <ThemeProvider theme={theme}>
-      <DemoBox text="this is a text"/> 
-    </ThemeProvider>
-}
 
 export default App;
--- samples/theming-app/src/demoComponent.jsx
+++ samples/theming-app/src/demoComponent.jsx
@@ -1,21 +1,28 @@
-import { Box, Typography } from '@material-ui/core';
-import { withTheme } from 'theming';
+import { Box, Typography } from "@material-ui/core";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const DemoBox = props => {
+const withStyle = withStyles((theme) => ({
+  box: {
+    color: theme.colors.red,
+    textAlign: "center",
+  },
+}));
+
+const DemoBox = (props) => {
   console.log("DemoBox props", props);
   return (
-  <Box style={props.theme}>
-    <Typography variant='h1'>This is a Demo Box</Typography>
-    <Typography variant='h2'>{props.text}</Typography>
-  </Box>
+    <Box className={props.classes.box}>
+      <Typography variant="h1">This is a Demo Box</Typography>
+      <Typography variant="h2">{props.text}</Typography>
+    </Box>
   );
-}
+};
 
 DemoBox.propTypes = {
-  text: PropTypes.string.isRequired
-}
+  text: PropTypes.string.isRequired,
+};
 
 // export default DemoBox
-export default withTheme(DemoBox);
+export default withStyle(DemoBox);
